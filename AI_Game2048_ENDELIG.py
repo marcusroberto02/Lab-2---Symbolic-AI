f1534506fc39c9a9a236e96fd85b5942feba8b55
marcusroberto02 <58505443+marcusroberto02@users.noreply.github.com>
2020-09-24 14:30:59 +0200

    Add files via upload

--- AI_Game2048_ENDELIG.py
+++ AI_Game2048_ENDELIG.py
@@ -25,7 +25,7 @@ for i in range(repetitions):
     done = False
     score = 0
     while not done:
-        action = random.choice(moves)
+        action = moves[np.random.randint(4)]
         (board, score), reward, done = sim.step(action)
     avg_score += score
 
@@ -33,28 +33,35 @@ avg_score /= repetitions
 
 # bestem gennemsnitlig score vha. monte carlo
 mon_avg_score = 0
-mon_repetitions = 100
-rep = 1
+mon_repetitions = 1
+rep = 1000
 for i in range(mon_repetitions):
     print(i)
     mon = Game2048()
     mon.reset()
     done = False
     score = 0
+    t = 0
     while not done:
+        if t % 10 == 0:
+            print(t)
+        t += 1
         # vælg bedste træk
         best_score = 0
         best_action = moves[0]
         for j in range(len(moves)):
+            temp = Game2048((mon.board, mon.score))
+            temp.reset()
             a_score = 0
-            for k in range(rep):
-                temp = Game2048((mon.board, mon.score))
-                temp.reset()
-                d = False
-                s = 0
+            eq = np.copy(mon.board)
+            (b, s), r, d = temp.step(moves[j])
+            comp = eq == b
+            if comp.all():
+                continue
+            for _ in range(rep):
                 while not d:
-                    action = random.choice(moves)
-                    (board, s), reward, d = temp.step(action)
+                    action = moves[np.random.randint(4)]
+                    (b, s), r, d = temp.step(action)
                 a_score += s
             a_score /= rep
             if a_score > best_score:
@@ -65,6 +72,8 @@ for i in range(mon_repetitions):
     mon_avg_score += score
 
 mon_avg_score /= mon_repetitions
+
+
 
 print("From random plays average score is:", avg_score)
 print("With Monte Carlo Search average score is:", mon_avg_score)
